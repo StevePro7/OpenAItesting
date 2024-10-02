30a04c87f20336bff0722dc520f1487bf2a9ef1c
StevePro7 <steveprozune@hotmail.com>
2024-10-02 21:06:04 +0100

    Sms

--- RetroCheatSheet/examples/README.md
+++ RetroCheatSheet/examples/README.md
@@ -6,3 +6,15 @@ source .venv/bin/activate
 
 pip install -r requirements.txt
 pip install --upgrade pip
+
+
+
+/home/stevepro/GitHub/StevePro9/OpenAItesting/RetroCheatSheet/examples/.venv/lib/python3.8/site-packages/retro/data/stable/AaahhRealMonsters-Genesis
+
+
+/.venv/lib/python3.8/site-packages/retro/data/stable/AaahhRealMonsters-Genesis
+
+
+
+TODO
+delete bar.py
---
+++ RetroCheatSheet/examples/copy_files.py
@@ -0,0 +1,35 @@
+import os.path
+import shutil
+
+
+def get_lines(dirX: str) -> list:
+    file: str = f"copy_{dirX}.txt"
+    with open(file, 'r') as fh:
+        lines = fh.readlines()
+    return lines
+
+
+def foo(dirX, extn: str) -> None:
+    files: list = get_lines(dirX)
+    for file in files:
+        file = file.strip()
+        bar(file, extn)
+        #print(file)
+
+
+def bar(file, extn: str):
+    path: str = os.path.abspath(os.path.dirname(__file__))
+    print(f"{file} beg")
+    srce: str = f"{path}/../data/stable/{file}/rom.{extn}"
+    #dest: str = f"{path}/../data/steven/{file}/rom.{extn}"
+    dest: str = f"{path}/.venv/lib/python3.8/site-packages/retro/data/stable/{file}/rom.{extn}"
+    shutil.copy(srce, dest)
+    #print(srce)
+    #print(dest)
+    print(f"{file} end")
+
+if __name__ == '__main__':
+    #bar("AlienSyndrome-Sms", "sms")
+    foo("Sms", "sms")
+
+
