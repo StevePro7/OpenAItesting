22d6a028b2da424801e72725cecba924faf0f824
StevePro7 <steveprozune@hotmail.com>
2024-10-03 21:17:48 +0100

    readme

--- RetroCheatSheet/examples/copy_files.py
+++ RetroCheatSheet/examples/copy_files.py
@@ -21,16 +21,12 @@ def bar(file, extn: str):
     path: str = os.path.abspath(os.path.dirname(__file__))
     print(f"{file} beg")
     srce: str = f"{path}/../data/stable/{file}/rom.{extn}"
-    #dest: str = f"{path}/../data/steven/{file}/rom.{extn}"
     dest: str = f"{path}/.venv/lib/python3.8/site-packages/retro/data/stable/{file}/rom.{extn}"
     shutil.copy(srce, dest)
-    #print(srce)
-    #print(dest)
     print(f"{file} end")
 
 if __name__ == '__main__':
-    # bar("AlienSyndrome-Sms", "sms")
-    # foo("Sms", "sms")
+    foo("Sms", "sms")
     foo("Genesis", "md")
 
 
